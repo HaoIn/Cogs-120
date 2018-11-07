e69631c591fc9ddbde5848cd9df0f70f532a5223
Hao-In Choi <hic003@ucsd.edu>
2018-11-07 15:33:47 -0800

    something

--- js/myCloset.js
+++ js/myCloset.js
@@ -5,9 +5,13 @@ $(document).ready(function() {
   $('#allTab, #topsTab,#bottomsTap, #shoesTap, #accessoriesTab').click(function(event) {
     $('#allTab, #topsTab,#bottomsTap, #shoesTap, #accessoriesTab').removeClass('tabActive');
     $(this).addClass('tabActive');
-
   });
 
+  $('#addButton').click(function(event) {
+    var itemImg = prompt("")
+  });
+//preload bunch of fake image
+//ask the people enter the data we want.
 })
 
 function editMode() {
