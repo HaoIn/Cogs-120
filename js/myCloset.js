2155f9eaeb7274421f36c3d8befb5d408220ba5d
Hao-In Choi <hic003@ucsd.edu>
2018-11-06 21:48:23 -0800

    some js

--- js/myCloset.js
+++ js/myCloset.js
@@ -1,3 +1,19 @@
-$('$(document).ready(function() {
+$(document).ready(function() {
+  console.log('hello world');
+  editMode();
 
-});
+  $('#allTab, #topsTab,#bottomsTap, #shoesTap, #accessoriesTab').click(function(event) {
+    $('#allTab, #topsTab,#bottomsTap, #shoesTap, #accessoriesTab').removeClass('tabActive');
+    $(this).addClass('tabActive');
+
+  });
+
+})
+
+function editMode() {
+  $('#editButton').click(function(event) {
+    $(this).hide();
+    $('#saveButton').fadeIn();
+    $('#addButton').fadeIn();
+  });
+}
