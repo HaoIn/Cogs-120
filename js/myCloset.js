97b449ef8d800b9fe32d57261efa37fb25eca556
Hao-In Choi <hic003@ucsd.edu>
2018-11-15 22:26:42 -0800

    done my closet

--- js/myCloset.js
+++ js/myCloset.js
@@ -1,25 +1,25 @@
 var complexData = [{
-    'name': 'blacksuptshirt',
+    'name': 'BlackSuptShirt',
     'url': 'img/blacksuptshirt.jpg',
     'type': 'tops'
   },
   {
-    'name': 'Eazy 350',
+    'name': 'Eazy350',
     'url': 'img/eazy.jpg',
     'type': 'shoes'
   },
   {
-    'name': 'Gucci Belt',
+    'name': 'GucciBelt',
     'url': 'img/belt.jpg',
     'type': 'accessories'
   },
   {
-    'name': 'White Sup T-Shirt',
+    'name': 'WhiteSupT-Shirt',
     'url': 'img/supreme_Shirt.jpg',
     'type': 'tops'
   },
   {
-    'name': 'Gucci Pant',
+    'name': 'GucciPant',
     'url': 'img/guccipants.jpg',
     'type': 'bottoms'
   },
@@ -29,22 +29,22 @@ var complexData = [{
     'type': 'accessories'
   },
   {
-    'name': 'Grey Eazy 350',
+    'name': 'GreyEazy350',
     'url': 'img/eazygrey.jpg',
     'type': 'shoes'
   },
   {
-    'name': 'Audemar Piguet',
+    'name': 'AudemarPiguet',
     'url': 'img/icedoutap.jpg',
     'type': 'accessories'
   },
   {
-    'name': 'Red Nike Pant',
+    'name': 'RedNikePant',
     'url': 'img/nikepants.jpeg',
     'type': 'bottoms'
   },
   {
-    'name': 'Black Nike Pant',
+    'name': 'BlackNikePant',
     'url': 'img/nike_pants.jpg',
     'type': 'bottoms'
   }
@@ -54,29 +54,60 @@ var complexData = [{
 
 $(document).ready(function() {
   console.log('hello world');
-  editMode();
 
-  $('#allTab, #topsTab,#bottomsTap, #shoesTap, #accessoriesTab').click(function(event) {
-    $('#allTab, #topsTab,#bottomsTap, #shoesTap, #accessoriesTab').removeClass('tabActive');
-    $(this).addClass('tabActive');
-  });
+    if (!(localStorage.key(0) === "itemData")){
+      localStorage.setItem('itemData', JSON.stringify(complexData));
+    }
 
-  var source   = $("#item-template").html();
+
+
+
+
+  var source = $("#item-template").html();
   var template = Handlebars.compile(source);
   var parentDiv = $(".row");
 
   for (var i = 0; i < complexData.length; i++) {
-    var curData = complexData[i];
-    var curHtml = template(curData);
+    var curData = JSON.parse(localStorage.getItem('itemData'));
+    var curHtml = template(curData[i]);
+    // need to load data from localStorage
     parentDiv.append(curHtml);
   }
   filterSelection("all");
+
+  $('#add').click(function(event) {
+    let myStorage = window.localStorage;
+    let existingData = JSON.parse(myStorage.getItem('itemData'));
+    let itemName = $('#item-name').val();
+    let url = $('#url').val();
+    let type = $('#types').val();
+    let item = {
+      'name': itemName,
+      'url': url,
+      'type': type
+    };
+    existingData.push(item);
+    myStorage.setItem('itemData', JSON.stringify(existingData));
+    reload();
+    filterSelection("all");
+  });
+
+
+
 })
 
-function editMode() {
-  $('#editButton').click(function(event) {
-    $(this).hide();
-    $('#saveButton').fadeIn();
-    $('#addButton').fadeIn();
-  });
+
+function reload() {
+  $('.row').empty();
+  var source = $("#item-template").html();
+  var template = Handlebars.compile(source);
+  var parentDiv = $(".row");
+  var curData = JSON.parse(localStorage.getItem('itemData'));
+  for (var i = 0; i < curData.length; i++) {
+    var curHtml = template(curData[i]);
+    console.log(curHtml);
+    parentDiv.append(curHtml);
+  }
+  filterSelection("all");
+  window.location.reload();
 }
